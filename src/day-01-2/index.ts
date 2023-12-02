262fe41ac4f1577da2d643e28b02745347fac8c7
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-12-02 09:04:34 +0800

    2023-01

---
+++ src/day-01-2/index.ts
@@ -0,0 +1,37 @@
+const digits = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9'
+}
+
+export function calibrateValue(v: string): number {
+  const values: string[] = v.split('')
+  let firstDigitIndex = values.findIndex(v => Number.isInteger(parseInt(v)))
+  let firstDigit = values[firstDigitIndex]
+  let lastDigitIndex = values.findLastIndex(v => Number.isInteger(parseInt(v)))
+  let lastDigit = values[lastDigitIndex]
+
+  if (firstDigitIndex === -1) firstDigitIndex = Infinity
+  if (lastDigitIndex === -1) lastDigitIndex = -Infinity
+
+  for (const d of Object.entries(digits)) {
+    const i = v.indexOf(d[0])
+    const li = v.lastIndexOf(d[0])
+    if (i !== -1 && i < firstDigitIndex) {
+      firstDigitIndex = i
+      firstDigit = d[1]
+    }
+    if (li !== -1 && li > lastDigitIndex) {
+      lastDigitIndex = li
+      lastDigit = d[1]
+    }
+  }
+
+  return parseInt(`${firstDigit}${lastDigit}`)
+}
---
+++ src/day-01-2/input.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from 'bun:test'
+import { calibrateValue } from './index'
+
+test('value should be 29', () => {
+  expect(calibrateValue('two1nine')).toBe(29)
+})
+
+test('value should be 83', () => {
+  expect(calibrateValue('eightwothree')).toBe(83)
+})
+
+const sum = (n: number[]) => n.reduce((p, c) => p + c, 0)
+
+test('answer should be 142', () => {
+  const testData: string[] = [
+    'two1nine',
+    'eightwothree',
+    'abcone2threexyz',
+    'xtwone3four',
+    '4nineeightseven2',
+    'zoneight234',
+    '7pqrstsixteen'
+  ]
+  
+  expect(sum(testData.map(calibrateValue))).toBe(281)
+})
+
+test('final answer should be ?', async () => {
+  const inputFile = await Bun.file('../day-01/input.txt').text()
+  const testData = inputFile.split('\n').filter(s => s.length > 0)
+
+  expect(sum(testData.map(calibrateValue))).toBe(54530)
+})
