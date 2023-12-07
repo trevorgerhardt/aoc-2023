aca9617bfb8dd416d199c67e359188fec2db97eb
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-12-07 13:38:21 +0800

    Clean up

--- packages/05/index.test.ts
+++ packages/05/index.test.ts
@@ -5,44 +5,43 @@ function parseFile (file: string) {
   const seeds = Array.from(seedLine.match(/\d+/g)!).map(s => +s)
 
   const maps = mapSections.map(section => {
+    const map: number[] = []
     const [, ...lines] = section.split('\n')
-    return lines.map(line => Array.from(line.match(/\d+/g)!).map(s => +s))
+    for (const line of lines) {
+      const [drs, srs, rl] = Array.from(line.match(/\d+/g)!).map(s => +s)
+      for (let i = srs; i < srs + rl; i++) {
+        map[i] = drs + (i - srs)
+      }
+    } 
+    return map
   })
 
   return [seeds, maps] as const
 }
 
-function getDestinationId(sourceId: number, map: number[][]) {
-  for (const [drs, srs, rl] of map) {
-    if (srs <= sourceId && (srs + rl) > sourceId) {
-      return drs + (sourceId - srs)
-    }
-  }
-  return sourceId
-}
-
-function closestLocation (seeds: number[], maps: number[][][]) {
+function closestLocation (seeds: number[], maps: number[][]) {
   let closest = Infinity
   for (const seed of seeds) {
     let id = seed
     for (const map of maps) {
-      id = getDestinationId(id, map)
+      id = map[id] ?? id
     }
     if (id < closest) closest = id
   }
   return closest
 }
 
-function closestLocationRanges (seeds: number[], maps: number[][][]) {
+function closestLocationRanges (seeds: number[], maps: number[][]) {
   let closest = Infinity
   for (let i = 0; i < seeds.length - 1; i += 2) {
     for (let seed = seeds[i]; seed < seeds[i] + seeds[i + 1]; seed++) {
       let id = seed
       for (const map of maps) {
-        id = getDestinationId(id, map)
+        id = map[id] ?? id
       }
       if (id < closest) closest = id
     }
+    console.log(`seed ${i} complete`)
   }
   return closest
 }
@@ -60,9 +59,10 @@ describe('test', async () => {
   })
 })
 
-describe('result', async () => {
+test('result', async () => {
   const inputFile = await Bun.file(`${import.meta.dir}/../../data/05-input.txt`).text()
   const [seeds, maps] = parseFile(inputFile)
   console.log('pt 1', closestLocation(seeds, maps))
   console.log('pt 2', closestLocationRanges(seeds, maps))
+  expect(Bun).toBeDefined()
 })
