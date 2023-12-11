bd157cae700b1020973db0cee53a71bada69e43a
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-12-11 16:05:25 +0800

    Day 11

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,3 +1,5 @@
+export type Coord = { x: number; y: number }
+
 export const sum = (n: number[]) => n.reduce((sum, num) => sum + num, 0)
 export const uniq = <T>(a: T[]): T[] => a.filter((v, i) => a.indexOf(v) === i)
 
