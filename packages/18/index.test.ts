497129e26b5bae82cda69b653c35c2011ce87dd9
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-12-18 14:07:39 +0800

    Day 18 Pt 2

--- packages/18/index.test.ts
+++ packages/18/index.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, test } from 'bun:test'
-import inside from 'point-in-polygon-hao'
-import { Coord, getInput, sumWith } from '../utils'
+import { getInput, print } from '../utils'
 
 const DAY = 18
 
@@ -22,20 +21,42 @@ U 2 (#7a21e3)
 `.trim()
 
 type Dir = 'U' | 'D' | 'L' | 'R'
-type Tile = '.' | '|' | '-' | '\\' | '/'
-type Beam = { x: number; y: number; d: Dir }
+
+function codeToDir (s: string): Dir {
+  switch (s) {
+    case '0': return 'R'
+    case '1': return 'D'
+    case '2': return 'L'
+    case '3': return 'U'
+  }
+  return 'U'
+}
 
 const parse = (input: string) =>
   input
     .split('\n')
     .map(r => {
       const [d, m, c] = r.split(' ')
+      const hexMeters = parseInt(c.slice(2, -2), 16)
+      const dirCode = c.slice(-2, -1)
       return {
-        d: d as Dir,
-        m: +m,
+        d: codeToDir(dirCode),
+        m: hexMeters,
         c: c.slice(1, -1) // remove parens
       }
     })
+
+function calcPolygonArea (p: [number, number][]): number {
+  let area = 0
+
+  for (let i = 0; i < p.length; i++) {
+    const j = (i + 1) % p.length
+    area += p[i][0] * p[j][1]
+    area -= p[i][1] * p[j][0]
+  }
+
+  return Math.abs(area / 2);
+}
 
 function calc(input: ReturnType<typeof parse>) {
   const trench: [number, number][] = [[0, 0]]
@@ -55,33 +76,28 @@ function calc(input: ReturnType<typeof parse>) {
     }
   }
 
-  let total = trench.length - 1
-  const polygon = [trench]
-  for (x = minX; x <= maxX; x++) {
-    for (y = minY; y <= maxY; y++) {
-      if (inside([x, y], polygon)) total++
-    }
-  }
+  print('trench dug. Now calculating interior...')
+  print('trench length', trench.length, 'x size', maxX - minX, 'y size', maxY - minY)
 
-  return total
+  return calcPolygonArea(trench) + ((trench.length - 1) / 2) + 1 
 }
 
 describe(`2023-${DAY}`, () => {
   describe('test', async () => {
-    test('pt 1 should be', () => {
+    test.skip('pt 1 should be', () => {
       expect(calc(parse(exampleInput1))).toBe(62)
     })
 
-    test.skip('pt 2 should be', () => {
-      expect(calc(parse(exampleInput1))).toBe(51)
+    test('pt 2 should be', () => {
+      expect(calc(parse(exampleInput1))).toBe(952408144115)
     })
   })
 
   test('result', async () => {
     const inputFile = await getInput(DAY)
     const parsedInput = parse(inputFile)
-    console.log('result:', calc(parsedInput))
-    // console.log('result 2:', maxEnergy(parsedInput))
+    // console.log('result:', calc(parsedInput))
+    console.log('result 2:', calc(parsedInput))
     expect(Bun).toBeDefined()
   })
 })
